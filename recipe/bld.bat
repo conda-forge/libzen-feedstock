212964fec07d30cee531ff1812bc4245d8380ce9
Andre Anjos <andre.dos.anjos@gmail.com>
2021-11-11 14:47:27 +0100

    Build shared libraries also on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,7 @@ rem Create Makefiles
 cmake -G Ninja ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D BUILD_SHARED_LIBS=ON ^
       -S Project\CMake ^
       -B build
 if errorlevel 1 exit /b 1
